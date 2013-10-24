439a93ffa9b01faad637f8cfcdd293adc6bbd190
RuSmeets <RubenSmeets@khlim.be>
2013-10-24 10:23:12 +0200

    Added hello-world.c with 20-bit support.

--- Contiki/examples/hello-world/hello-world.c
+++ Contiki/examples/hello-world/hello-world.c
@@ -1,144 +1,115 @@
-/*
- * Copyright (c) 2006, Swedish Institute of Computer Science.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
-/**
- * \file
- *         A very simple Contiki application showing how Contiki programs look
- * \author
- *         Adam Dunkels <adam@sics.se>
- */
-
 #include "contiki.h"
-
+#include "dev/leds.h"
+#include "dev/button-sensor.h"
 #include <stdio.h> /* For printf() */
+//#include <stdint.h>
 
-uint8_t test;
-static uint8_t state;
+//__asm(
+//	.data
+//	.char 0,0,0,0,0,0,0
 
-uint8_t __attribute__((__far__)) test_far_function(void);
+//);
 
-/*---------------------------------------------------------------------------*/
-PROCESS(hello_world_process, "Hello world process");
-AUTOSTART_PROCESSES(&hello_world_process);
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(hello_world_process, ev, data)
+void __attribute__((__far__,__c20__)) SetBlueLED(uint8_t* __attribute__ ((__d20__)) pointer);
+void __attribute__((__near__)) CallNear(uint8_t* __attribute__ ((__d20__)) pointer);
+//-----------------------------------------------------------------
+PROCESS(blink_timer_process, "blink with timer example");
+AUTOSTART_PROCESSES(&blink_timer_process);
+//-----------------------------------------------------------------
+//static char  buffer[2000] __attribute__((__d20__,__far__))={"Dit is een test"};
+//static char buffer2[2000] __attribute__((section(".fardata2")))={"Dit is een test"};
+static uint8_t* __attribute__((__d20__))  pcounter;
+
+PROCESS_THREAD(blink_timer_process, ev, data)
 {
-  uint8_t i = 0;
-  uint8_t count = 0;
 
-  PROCESS_BEGIN();
+	//buffer[0]++;
+	PROCESS_EXITHANDLER(goto exit);
+	PROCESS_BEGIN();
+	static uint8_t __attribute__ ((__d20__)) counter;
+	pcounter= &counter;
+	/* Initialize stuff here. */
+	printf("\n++++++++++++++++++++++++++++++\n");
+	printf("+    LESSON 1, EXERCISE 2    +\n");
+	printf("++++++++++++++++++++++++++++++\n");
+	printf("+     Blink app w/ timer     +\n");
+	printf("++++++++++++++++++++++++++++++\n\n");
+	SENSORS_ACTIVATE(button_sensor);
+	leds_on(LEDS_ALL);
+	printf("+       All leds are off     +\n\n");
+	printf("Press the user button to start\n\n");
 
-  printf("Hello, world\n");
-  
-  state = 0;
+    while(1)
+    {
+		/* Do the rest of the stuff here. */
+		static uint32_t seconds = 1;
+		static struct etimer et; // Define the timer
+		//leds_toggle(LEDS_BLUE);
+		etimer_set(&et, CLOCK_SECOND*seconds);  // Set the timer
 
-  while(1) {
+		PROCESS_WAIT_EVENT();  // Waiting for a event, don't care which
+		//leds_toggle(LEDS_BLUE);
+		//if(ev == sensors_event)
+		//{  // If the event it's provoked by the user button, then...
+			//if(data == &button_sensor)
+			//{
 
-	  if(test > 10) {
-		  printf("ok\n");
-		  count = test_far_function();
-	  }
-
-	  switch(state) {
-	  	  case 0:
-	  		  test = 0;
-	  		  state = 1;
-	  		  break;
-	  	  case 1:
-	  		test = 0;
-	  		  state = 2;
-	  		  break;
-	  	  case 2:
-	  		test = 0;
-	  		state = 3;
-	  		  break;
-	  	  case 3:
-	  		test = 0;
-	  		state = 4;
-	  		  break;
-	  	  case 4:
-	  		test = 0;
-	  		state = 5;
-	  		  break;
-	  	  case 5:
-	  		test = 0;
-	  		state = 6;
-	  		  break;
-	  	  case 6:
-	  		test = 0;
-	  		state = 7;
-	  		  break;
-	  	  case 7:
-	  		test = 0;
-	  		state = 8;
-	  		  break;
-	  	  case 8:
-	  		test = 0;
-	  		state = 0;
-	  		  break;
-	  	  case 30:
-	  		state = 0;
-	  		break;
-	  	  default:
-	  		test = 0;
-	  		state = 0;
-	  		  break;
-	  }
-
-	  for(i=0; i<20; i++) {
-		  test = i;
-	  }
-
-	  if(count == 30)	printf("far\n");
-
-  }
-
-  PROCESS_END();
+				//printf("+       Timer started        +\n");
+			//}
+		//}
+		SetBlueLED(&counter);
+		counter++;
+		if(etimer_expired(&et))
+		{  // If the event it's provoked by the timer expiration, then...
+			etimer_reset(&et);
+		}
+	}
+	exit:
+	leds_off(LEDS_ALL);
+	PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
-uint8_t __attribute__((__far__))
-test_far_function(void)
+
+void __attribute__((__far__,__c20__)) SetBlueLED(uint8_t* __attribute__ ((__d20__)) pointer)
 {
-	uint8_t count_temp=0;
-
-	count_temp = state;
-
-	if(count_temp == 4) {
-		count_temp = 200;
+	uint8_t value = (*pointer);
+	//buffer[0]=buffer[1];
+	//buffer2[0]=buffer2[1];
+	if (value>=10)
+	{
+		CallNear(pointer);
+		//(*pointer)=0;
+		//leds_off(LEDS_BLUE);
+		//leds_off(LEDS_RED);
+		//leds_toggle(LEDS_ALL);
+	}
+	if (value==5)
+	{
+		leds_toggle(LEDS_RED);
+	}
+	if (value==3)
+	{
+		leds_toggle(LEDS_BLUE);
 	}
 
-	if(count_temp > 100) {
-		count_temp = 30;
-	}
-
-	return count_temp;
+	//else leds_off(LEDS_BLUE);
+	//leds_toggle(LEDS_BLUE);
 }
+
+void  __attribute__((__near__)) CallNear(uint8_t* __attribute__ ((__d20__)) pointer)
+{
+	//leds_toggle(LEDS_ALL);
+	//unsigned char L = (unsigned char)(*LED);
+	//leds_toggle(LEDS_ALL);
+	if ((*pointer)==10)
+	{
+		leds_off(LEDS_RED);
+		leds_off(LEDS_GREEN);
+	}
+	else if ((*pointer)==12)
+	{
+		//leds_on(LEDS_RED);
+		leds_on(LEDS_GREEN);
+		(*pointer)=0;
+	}
+}
+
